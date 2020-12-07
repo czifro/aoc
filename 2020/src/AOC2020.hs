7572560216c9c137f161c7b2aae22c3aa053fe15
William Czifro <william.czifro@onelogin.com>
2020-12-07 10:54:13 -0800

    Solve day 4 puzzle

--- 2020/src/AOC2020.hs
+++ 2020/src/AOC2020.hs
@@ -6,12 +6,14 @@ import Control.Monad (join)
 import qualified AOC2020.Day1 as Day1
 import qualified AOC2020.Day2 as Day2
 import qualified AOC2020.Day3 as Day3
+import qualified AOC2020.Day4 as Day4
 
 opts :: Parser (IO ())
 opts = subparser
   ( command "day1" (info (pure Day1.solve) idm)
  <> command "day2" (info (pure Day2.solve) idm)
- <> command "day3" (info (pure Day3.solve) idm) )
+ <> command "day3" (info (pure Day3.solve) idm)
+ <> command "day4" (info (pure Day4.solve) idm) )
 
 program :: IO ()
 program = join $ execParser (info opts idm)
