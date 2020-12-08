886aa6ffebe961d11e7362e56002798558b429ce
William Czifro <william.czifro@onelogin.com>
2020-12-08 14:01:42 -0800

    Solve day5 puzzle

--- 2020/src/AOC2020.hs
+++ 2020/src/AOC2020.hs
@@ -6,13 +6,15 @@ import qualified AOC2020.Day1 as Day1
 import qualified AOC2020.Day2 as Day2
 import qualified AOC2020.Day3 as Day3
 import qualified AOC2020.Day4 as Day4
+import qualified AOC2020.Day5 as Day5
 
 opts :: Parser (IO ())
 opts = subparser
   ( command "day1" (info (pure Day1.solve) idm)
  <> command "day2" (info (pure Day2.solve) idm)
  <> command "day3" (info (pure Day3.solve) idm)
- <> command "day4" (info (pure Day4.solve) idm) )
+ <> command "day4" (info (pure Day4.solve) idm)
+ <> command "day5" (info (pure Day5.solve) idm) )
 
 program :: IO ()
 program = join $ execParser (info opts idm)
---
+++ 2020/test/AOC2020/Day5Spec.hs
@@ -0,0 +1,21 @@
+module AOC2020.Day5Spec (spec) where
+
+import Test.Hspec
+import AOC2020.Day5 ( calculateSeatId
+                   )
+
+testInput :: [String]
+testInput = [ "FBFBBFFRLR"
+            , "BFFFBBFRRR"
+            , "FFFBBBFRRR"
+            , "BBFFBBFRLL"
+            ]
+
+expected1 :: [Int]
+expected1 = [357, 567, 119, 820]
+
+spec :: Spec
+spec = do
+  describe "<AOC2020.Day5>" $ do
+    it "should calculate correct seat id" $ do
+      map calculateSeatId testInput `shouldBe` expected1
