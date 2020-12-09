95e0c63d819ff6a7600dc9c3b197ff24f69a9335
William Czifro <william.czifro@onelogin.com>
2020-12-08 16:15:47 -0800

    Solve day6 puzzle

--- 2020/src/AOC2020.hs
+++ 2020/src/AOC2020.hs
@@ -7,6 +7,7 @@ import qualified AOC2020.Day2 as Day2
 import qualified AOC2020.Day3 as Day3
 import qualified AOC2020.Day4 as Day4
 import qualified AOC2020.Day5 as Day5
+import qualified AOC2020.Day6 as Day6
 
 opts :: Parser (IO ())
 opts = subparser
@@ -14,7 +15,8 @@ opts = subparser
  <> command "day2" (info (pure Day2.solve) idm)
  <> command "day3" (info (pure Day3.solve) idm)
  <> command "day4" (info (pure Day4.solve) idm)
- <> command "day5" (info (pure Day5.solve) idm) )
+ <> command "day5" (info (pure Day5.solve) idm)
+ <> command "day6" (info (pure Day6.solve) idm) )
 
 program :: IO ()
 program = join $ execParser (info opts idm)
---
+++ 2020/test/AOC2020/Day6Spec.hs
@@ -0,0 +1,24 @@
+module AOC2020.Day6Spec (spec) where
+
+import Test.Hspec
+import AOC2020.Day6 ( countUniqueQuestionsAnswered
+                    , countSameQuestionsAnswered
+                    )
+
+testInput :: String
+testInput =
+  "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb"
+
+expected1 :: Int
+expected1 = 11
+
+expected2 :: Int
+expected2 = 6
+
+spec :: Spec
+spec = do
+  describe "<AOC2020.Day6>" $ do
+    it "should correctly count unique questions answered" $ do
+      countUniqueQuestionsAnswered testInput `shouldBe` expected1
+    it "should correctly count same questions answered" $ do
+      countSameQuestionsAnswered testInput `shouldBe` expected2
